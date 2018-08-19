b64015b9cc00acc7d952eb336788de6b5d58c9ba
qinbin52qiul <2538323140@qq.com>
2018-08-19 19:01:35 +0800

    Add Shopand mainShowon the homepage

--- project-axf/axf/views.py
+++ project-axf/axf/views.py
@@ -1,17 +1,29 @@
 from django.shortcuts import render
-from .models import Wheel, Nav,Mustbuy
+from .models import Wheel, Nav, Mustbuy, Shop, MainShow
 
 
 def home(request):
     wheelsList = Wheel.objects.all()
     navList = Nav.objects.all()
     mustbuyList = Mustbuy.objects.all()
-    return render(request, 'axf/home.html', {
-        "title": "主页",
+    shopList = Shop.objects.all()
+    shop1 = shopList[0]
+    shop2 = shopList[1:3]
+    shop3 = shopList[3:7]
+    shop4 = shopList[7:11]
+    mainList = MainShow.objects.all()
+    context = {
+        "title": '主页',
         "wheelsList": wheelsList,
         "navList": navList,
         "mustbuyList": mustbuyList,
-    })
+        "shop1": shop1,
+        "shop2": shop2,
+        "shop3": shop3,
+        "shop4": shop4,
+        "mainList": mainList,
+    }
+    return render(request, 'axf/home.html', context=context)
 
 
 def market(request):
